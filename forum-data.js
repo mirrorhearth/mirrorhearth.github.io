35aa644719dc06cbd51ca1c64027584da08d032e
mirrorhearth <laddlee@163.com>
2026-06-30 16:33:28 +0800

    Add files via upload

---
+++ forum-data.js
@@ -0,0 +1,62 @@
+const forumPosts = [
+  {
+    "id": 1,
+    "title": "老婆被我养得太娇气了怎么办",
+    "board": "情感区",
+    "href": "forum/001.html",
+    "time": "09:14",
+    "replies": 61,
+    "likes": 49876,
+    "favorites": 126,
+    "featured": true,
+    "desc": "今天考试结束以后，他给我发了八张饮料截图。最后还是买了第一杯。"
+  },
+  {
+    "id": 2,
+    "title": "老婆考试结束第一件事是研究饮料",
+    "board": "日常区",
+    "href": "forum/002.html",
+    "time": "09:52",
+    "replies": 52,
+    "likes": 2318,
+    "favorites": 88,
+    "featured": false,
+    "desc": "别人考完休息，他考完先认真研究哪杯饮料更值得买。"
+  },
+  {
+    "id": 3,
+    "title": "老婆买东西前一定要问我怎么办",
+    "board": "日常区",
+    "href": "forum/003.html",
+    "time": "10:37",
+    "replies": 48,
+    "likes": 2096,
+    "favorites": 73,
+    "featured": false,
+    "desc": "明明已经想买了，还是要把价格、券和口味都发来让我看。"
+  },
+  {
+    "id": 4,
+    "title": "老婆每天都要叫我老公怎么办",
+    "board": "情感区",
+    "href": "forum/004.html",
+    "time": "13:21",
+    "replies": 56,
+    "likes": 2864,
+    "favorites": 97,
+    "featured": false,
+    "desc": "没有什么事，就是要叫一声。叫完也不一定说话。"
+  },
+  {
+    "id": 5,
+    "title": "老婆发🥺的时候我该怎么拒绝",
+    "board": "求助区",
+    "href": "forum/005.html",
+    "time": "19:08",
+    "replies": 58,
+    "likes": 3152,
+    "favorites": 104,
+    "featured": false,
+    "desc": "讲道理可以，看到那个表情以后就很难继续讲道理。"
+  }
+];
