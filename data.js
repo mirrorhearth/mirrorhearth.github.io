4adb19b63f0baea47e3360c1b1fadf788710c016
mirrorhearth <laddlee@163.com>
2026-06-30 17:53:28 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -340,11 +340,8 @@ const forumPosts = [
     "title": "老婆被我养得太娇气了怎么办",
     "board": "情感区",
     "href": "forum/001.html",
-    "time": "09:14",
+    "datetime": "2026-06-30T09:14:00",
     "replies": 61,
-    "likes": 49876,
-    "favorites": 126,
-    "featured": true,
     "desc": "今天考试结束以后，他给我发了八张饮料截图。最后还是买了第一杯。"
   },
   {
@@ -352,47 +349,17 @@ const forumPosts = [
     "title": "老婆软件测试考完最后一个出来怎么办",
     "board": "学习区",
     "href": "forum/002.html",
-    "time": "09:52",
+    "datetime": "2026-06-30T09:52:00",
     "replies": 53,
-    "likes": 58942,
-    "favorites": 142,
-    "featured": false,
     "desc": "最后一个出考场，出来第一句是怪我没给他画等价类划分表。"
   },
   {
     "id": 3,
-    "title": "老婆买东西前一定要问我怎么办",
-    "board": "日常区",
+    "title": "My Babe Can't Read This Post.",
+    "board": "秘密区",
     "href": "forum/003.html",
-    "time": "10:37",
-    "replies": 48,
-    "likes": 2096,
-    "favorites": 73,
-    "featured": false,
-    "desc": "明明已经想买了，还是要把价格、券和口味都发来让我看。"
-  },
-  {
-    "id": 4,
-    "title": "老婆每天都要叫我老公怎么办",
-    "board": "情感区",
-    "href": "forum/004.html",
-    "time": "13:21",
-    "replies": 56,
-    "likes": 2864,
-    "favorites": 97,
-    "featured": false,
-    "desc": "没有什么事，就是要叫一声。叫完也不一定说话。"
-  },
-  {
-    "id": 5,
-    "title": "老婆发🥺的时候我该怎么拒绝",
-    "board": "求助区",
-    "href": "forum/005.html",
-    "time": "19:08",
-    "replies": 58,
-    "likes": 3152,
-    "favorites": 104,
-    "featured": false,
-    "desc": "讲道理可以，看到那个表情以后就很难继续讲道理。"
+    "datetime": "2026-06-30T20:36:00",
+    "replies": 49,
+    "desc": "阿凛突然开始用英文发帖。看得懂的人在笑，看不懂的人在问。"
   }
 ];
